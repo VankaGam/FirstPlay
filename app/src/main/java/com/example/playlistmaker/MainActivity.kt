98f889b5d9cae0b8ed4da15fe88717435ca49977
VankaGam <lumiamicrosoft@yandex.ru>
2024-11-27 15:08:25 +0300

    amendments

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -9,18 +10,19 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val preferences: SharedPreferences = getSharedPreferences("app_preferences", MODE_PRIVATE)
+        val isDarkThemeEnabled = preferences.getBoolean("isDarkTheme", false)
+        updateTheme(isDarkThemeEnabled)
         setContentView(R.layout.activity_main)
-
         val displayButton1 = findViewById<LinearLayout>(R.id.setting)
         val displayButton2 = findViewById<LinearLayout>(R.id.poisk)
         val displayButton3 = findViewById<LinearLayout>(R.id.media)
-
         displayButton1.setOnClickListener {
             val displayIntent1 = Intent(this, SettingActivity::class.java)
             startActivity(displayIntent1)
@@ -33,6 +35,12 @@ class MainActivity : AppCompatActivity() {
             val displayIntent3 = Intent(this, MediaLibraryActivity::class.java)
             startActivity(displayIntent3)
         }
-
+    }
+    private fun updateTheme(isDarkTheme: Boolean) {
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -6,23 +6,46 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var backButton: ImageButton
+    private lateinit var themeSwitch: SwitchCompat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+
+        themeSwitch = findViewById(R.id.controlp)
         backButton = findViewById(R.id.back)
+        val preferences = getSharedPreferences("app_preferences", MODE_PRIVATE)
+        val isDarkThemeEnabled = preferences.getBoolean("isDarkTheme", false)
+        themeSwitch.isChecked = isDarkThemeEnabled
+        updateTheme(isDarkThemeEnabled)
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            updateTheme(isChecked)
+            preferences.edit().putBoolean("isDarkTheme", isChecked).apply()
+        }
+        backButton.setOnClickListener {
+            finish()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        backButton.setOnClickListener {
-            finish()
+    }
+
+    private fun updateTheme(isDarkTheme: Boolean) {
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 }

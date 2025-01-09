7a895224bbc55320d14ad482c64997a215982e4a
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-01-09 19:52:13 +0300

    Revert "Sprint10"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,41 +1,20 @@
 package com.example.playlistmaker
 
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val preferences: SharedPreferences = getSharedPreferences("app_preferences", MODE_PRIVATE)
-        val isDarkThemeEnabled = preferences.getBoolean("isDarkTheme", false)
-        updateTheme(isDarkThemeEnabled)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val displayButton1 = findViewById<LinearLayout>(R.id.setting)
-        val displayButton2 = findViewById<LinearLayout>(R.id.poisk)
-        val displayButton3 = findViewById<LinearLayout>(R.id.media)
-        displayButton1.setOnClickListener {
-            val displayIntent1 = Intent(this, SettingActivity::class.java)
-            startActivity(displayIntent1)
-        }
-        displayButton2.setOnClickListener {
-            val displayIntent2 = Intent(this, SearchActivity::class.java)
-            startActivity(displayIntent2)
-        }
-        displayButton3.setOnClickListener {
-            val displayIntent3 = Intent(this, MediaLibraryActivity::class.java)
-            startActivity(displayIntent3)
-        }
-    }
-    private fun updateTheme(isDarkTheme: Boolean) {
-        if (isDarkTheme) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }

e77308e92ddd2792d9118904388cd274c154e14e
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-03-02 21:11:01 +0300

    Sprint13 Vse konec, mochno na proverku

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.google.android.material:material:1.11.0")
+    implementation ("jp.wasabeef:glide-transformations:4.3.0")
 }
--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -6,6 +6,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import model.Track
 
 class PlayerActivity : AppCompatActivity() {

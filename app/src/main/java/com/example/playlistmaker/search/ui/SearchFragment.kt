5e101029729b536150fd52b3c798d5939f2af537
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-06-05 23:19:14 +0300

    Spint 19/

--- app/src/main/java/com/example/playlistmaker/search/ui/SearchFragment.kt
+++ app/src/main/java/com/example/playlistmaker/search/ui/SearchFragment.kt
@@ -9,13 +9,11 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentSearchBinding
-import com.example.playlistmaker.search.domain.model.Track
 import com.example.playlistmaker.search.ui.viewmodel.SearchViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 

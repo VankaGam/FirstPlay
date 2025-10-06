5f75aa09526930eb47b83eb73fbde7cd8a244837
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-10-06 21:59:31 +0300

    creat vetka

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,10 @@ android {
         checkReleaseBuilds = false
         abortOnError = false
     }
+    buildFeatures { compose = true }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
 }
 
 dependencies {
@@ -77,4 +81,15 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
